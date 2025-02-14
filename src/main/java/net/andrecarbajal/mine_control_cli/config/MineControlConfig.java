d502f45178735fec2dd3e4263e73d65c10174e7a
anviaan <andre.carbajal.vargas@gmail.com>
2025-02-13 21:14:09 -0500

    Improve initial folder creations

--- src/main/java/net/andrecarbajal/mine_control_cli/config/MineControlConfig.java
+++ src/main/java/net/andrecarbajal/mine_control_cli/config/MineControlConfig.java
@@ -1,6 +1,5 @@
 package net.andrecarbajal.mine_control_cli.config;
 
-import lombok.extern.slf4j.Slf4j;
 import net.andrecarbajal.mine_control_cli.util.FileUtil;
 import net.andrecarbajal.mine_control_cli.util.ProgressBar;
 import org.jline.terminal.Terminal;
@@ -10,18 +9,35 @@ import org.springframework.context.annotation.Configuration;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Slf4j
 @Configuration
 public class MineControlConfig {
     @Bean
     public String init() {
-        Path path = FileUtil.getMineControlCliFolder();
-        if (Files.notExists(path)) {
+        Path mineControlCliFolder = FileUtil.getMineControlCliFolder();
+        Path serverInstancesFolder = FileUtil.getServerInstancesFolder();
+        Path serverBackupsFolder = FileUtil.getServerBackupsFolder();
+
+        if (Files.notExists(mineControlCliFolder)) {
             try {
-                Files.createDirectories(path);
+                Files.createDirectories(mineControlCliFolder);
             } catch (Exception e) {
-                log.error("Error creating mine-control-cli folder", e);
                 throw new RuntimeException("Error creating mine-control-cli folder", e);
+            }
+        }
+
+        if (Files.notExists(serverInstancesFolder)) {
+            try {
+                Files.createDirectories(serverInstancesFolder);
+            } catch (Exception e) {
+                throw new RuntimeException("Error creating server instances folder", e);
+            }
+        }
+
+        if (Files.notExists(serverBackupsFolder)) {
+            try {
+                Files.createDirectories(serverBackupsFolder);
+            } catch (Exception e) {
+                throw new RuntimeException("Error creating server backups folder", e);
             }
         }
 
