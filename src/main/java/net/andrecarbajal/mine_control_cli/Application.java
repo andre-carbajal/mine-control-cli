a371f1d88441f3474280588c82180d9b0ef72437
andre-carbajal <carbajalvargasandre@gmail.com>
2025-02-22 10:01:49 -0500

    Bump version to 0.1.0

--- src/main/java/net/andrecarbajal/mine_control_cli/Application.java
+++ src/main/java/net/andrecarbajal/mine_control_cli/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static final String APP_FOLDER_NAME = "mine-control-cli";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
